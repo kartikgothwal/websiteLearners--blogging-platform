8b9411d32750e252f6a8b1a887bf26764d2fcfef
Kartik Gothwal <kartikgothwal28@gmail.com>
2023-09-29 16:44:37 +0530

    Task Completed

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AuthController = require("../controller/auth");
 
 router
-  .post("/users/register", AuthController.register)
-  .post("/users/login", AuthController.login);
+  .post("/user/register", AuthController.register)
+  .post("/user/login", AuthController.login);
 
 exports.router = router;
